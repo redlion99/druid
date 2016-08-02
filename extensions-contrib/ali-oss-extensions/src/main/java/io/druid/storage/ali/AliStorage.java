a30ed1945ef25cffb6678fef1b9e776b840fe822
郑跃 <zhengyue@yinhongjundeMacBook-Pro.local>
2016-08-02 11:50:23 +0800

    add log

--- extensions-contrib/ali-oss-extensions/src/main/java/io/druid/storage/ali/AliStorage.java
+++ extensions-contrib/ali-oss-extensions/src/main/java/io/druid/storage/ali/AliStorage.java
@@ -2,6 +2,8 @@ package io.druid.storage.ali;
 
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.model.OSSObject;
+import com.metamx.common.logger.Logger;
+import io.druid.firehose.ali.StaticAliOssFirehoseFactory;
 
 import java.io.InputStream;
 
@@ -11,6 +13,7 @@ import java.io.InputStream;
 public class AliStorage {
     final AliDataSegmentConfig config;
     final OSSClient client;
+    private static final Logger log = new Logger(AliStorage.class);
 
     public AliStorage(AliDataSegmentConfig config) {
         this.config = config;
@@ -18,7 +21,9 @@ public class AliStorage {
     }
 
     public InputStream openStream(String file_name){
+
         OSSObject ossObject = this.client.getObject(this.config.getBucket(), file_name);
+        log.info("read oss object from ali oss : file_name -- [%s], content length -- [%s]", file_name,  ossObject.getObjectMetadata().getContentLength());
         return ossObject.getObjectContent();
     }
 }
